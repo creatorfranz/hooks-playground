aaf551626c71846dd968d2c3eb5b2dd6c89ef20f
Franz Evangelista <franz@iona.fi>
2024-07-05 05:20:51 +0800

    chore: add geolocation func

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGeolocation } from "@uidotdev/usehooks";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const state = useGeolocation();
+
+  if (state.loading) {
+    return <p>Loading... Please enable permissions.</p>;
+  }
+
+  if (state.error) {
+    return <p>Enable permissions to access your location data.</p>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {state.latitude} {state.longitude}
     </>
-  )
+  );
 }
-
-export default App
